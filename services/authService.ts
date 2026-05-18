a4bb0dd7d280c7cb8ea23606b97fe45778c30ee7
sebascordovaaa <sebascordovalaguna@icloud.com>
2026-05-18 15:19:59 +0200

    member select

--- services/authService.ts
+++ services/authService.ts
@@ -1,8 +1,7 @@
 // const API_URL = "http://192.168.1.64:3000/api"; // Dev
 const API_URL = "http://192.168.0.14:3000/api"; // Cordova
   
-// import { API_URL } from "../constants/api";fffffffffffaa
-
+// import { API_URL } from "../constants/api";
 type LoginResponse = {
   token: string;
 };
